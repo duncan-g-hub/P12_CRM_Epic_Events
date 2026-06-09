004fe8a26128ee7e4667a7386e48cb607e9400bc
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-06-09 14:28:11 +0200

    Mise en place de la connection à la base de donnée + gestion des tables

---
+++ main.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Base, Collaborateur, Client, Contrat, Evenement
+
+
+
+
+engine = create_engine(
+    "mysql+mysqlconnector://admin:admin@localhost/epic_events_CRM",
+    echo=True
+)
+
+# Session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# Créer les tables
+# Base.metadata.drop_all(engine)   # supprime toutes les tables
+Base.metadata.create_all(engine)
