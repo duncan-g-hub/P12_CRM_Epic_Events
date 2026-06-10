194e78dc8537d9ed5a64dbcd917b0f3ef32b212a
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-06-10 14:20:42 +0200

    refactor names

---
+++ controllers/contracts.py
@@ -0,0 +1,10 @@
+from models import Contrat
+from main import session
+
+
+
+def creer_contrat():
+    pass
+
+
+# date_dernier_contact pour le client à mettre à jour
---
+++ controllers/events.py
@@ -0,0 +1,12 @@
+from models import Evenement
+from main import session
+
+
+def creer_evenement():
+    pass
+
+
+
+
+
+# date_dernier_contact pour le client à mettre à jour
