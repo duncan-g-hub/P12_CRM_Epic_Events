9051805a3aedd7290ef04e70589aa72904f7e46b
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-06-10 14:40:38 +0200

    Création des roles si inexistant

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from database import engine
+from database import engine, session
 
-from models.models import Base, Collaborator, Customer, Contract, Event
+from models.models import Base, Role, Collaborator, Customer, Contract, Event
 
 
 
 # Créer les tables
 # Base.metadata.drop_all(engine)   # supprime toutes les tables
 Base.metadata.create_all(engine)
+
+# Alimenter les rôles si la table est vide
+if not session.query(Role).first():
+    session.add_all([
+        Role(name="commercial"),
+        Role(name="support"),
+        Role(name="gestion"),
+    ])
+    session.commit()
