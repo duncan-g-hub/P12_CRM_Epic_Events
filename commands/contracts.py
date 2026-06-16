a0f06701d51d3808441ac2eb32282fc479eb3e5c
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-06-16 09:33:07 +0200

    add commands to create contract

---
+++ commands/contracts.py
@@ -0,0 +1,43 @@
+import click
+
+from controllers.contracts import create_contract
+from controllers.customers import get_customers, get_customer_name
+from controllers.collaborators import get_commercials
+
+
+@click.group("contracts")
+def contracts_group():
+    """Gestion des contrats"""
+    pass
+
+
+customers, customer_ids = get_customers()
+commercials, commercial_ids = get_commercials()
+
+@contracts_group.command("create")
+@click.option(
+    "--customer-id",
+    prompt= f"\nClients disponibles :\n{customers}\n\nN° id du client",
+    type=click.Choice(customer_ids),
+)
+@click.option(
+    "--commercial-id",
+    prompt= f"\nCommerciaux disponibles :\n{commercials}\n\nN° id du commercial",
+    type=click.Choice(commercial_ids),
+)
+@click.option("--total-amount", prompt="Montant total", type=float)
+@click.option("--ammount-to-pay",  prompt="Reste à payer", type=float)
+@click.option("--signed",  prompt="Contrat signé ?", type=click.BOOL)
+
+
+@click.pass_context
+def create(ctx, customer_id, commercial_id, total_amount, amount_to_pay, signed):
+    """Créer un contrat"""
+    token = ctx.obj["token"]
+    try:
+        contract = create_contract(token, customer_id, commercial_id, total_amount, amount_to_pay, signed)
+        customer_name = get_customer_name(contract.customer_id)
+        click.echo(click.style(f"Contrat pour le client {customer_name} créé.", fg="green"))
+
+    except PermissionError as e:
+        click.echo(click.style(f"{e}", fg="red"))
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from models.models import Base, Role, Collaborator, Customer, Contract, Event
 from commands.auth import login_command, logout_command
 from commands.collaborators import collaborators_group
 from commands.customers import customers_group
+from commands.contracts import contracts_group
 from token_storage import load_token
 
 
@@ -35,6 +36,7 @@ cli.add_command(login_command)
 cli.add_command(logout_command)
 cli.add_command(collaborators_group)
 cli.add_command(customers_group)
+cli.add_command(contracts_group)
 
 if __name__ == "__main__":
     cli()
